41176f544ff6b8732502fe3b0a0658c0ee2c6bd0
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2019-11-18 19:15:42 -0500

    PubSub to broadcast editor updates

--- lib/markdown_live_web/live/editor.ex
+++ lib/markdown_live_web/live/editor.ex
@@ -16,16 +16,30 @@ defmodule MarkdownLiveWeb.Editor do
     """
   end
 
-  def mount(%{id: id}, socket) do
-    # Connect to Post by id
-    post = PostServer.summary(id)
-
-    {:ok, assign(socket, post)}
+  def mount(%{id: post_id}, socket) do
+    :ok = Phoenix.PubSub.subscribe(MarkdownLive.PubSub, post_id)
+    {:ok, assign_post(socket, post_id)}
   end
 
-  def handle_event("update", %{"value" => raw} = body, socket) do
-    id = "whatnow"
-    %{rendered: rendered_html} = PostServer.update(id, %{raw: raw})
-    {:noreply, assign(socket, rendered: rendered_html)}
+  def handle_event("update", %{"value" => raw}, socket) do
+    post_id = socket.assigns.post_id
+    PostServer.update(post_id, %{raw: raw})
+    :ok = Phoenix.PubSub.broadcast(MarkdownLive.PubSub, post_id, :update)
+    {:noreply, assign_post(socket)}
+  end
+  
+  def handle_info(:update, socket) do
+    {:noreply, assign_post(socket)}
+  end
+
+  defp assign_post(socket, post_id) do
+    socket
+    |> assign(post_id: post_id)
+    |> assign_post()
+  end
+
+  defp assign_post(%{assigns: %{post_id: post_id}} = socket) do
+    post = PostServer.summary(post_id)
+    assign(socket, rendered: post.rendered, raw: post.raw)
   end
 end
