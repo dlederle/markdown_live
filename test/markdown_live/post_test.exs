c8ca2eff5ae94f02a40b69223c2f93655adf05e3
Dylan Lederle-Ensign <dylan.lederle-ensign@viget.com>
2019-12-02 13:30:42 -0500

    Update post_test

--- test/markdown_live/post_test.exs
+++ test/markdown_live/post_test.exs
@@ -2,20 +2,9 @@ defmodule MarkdownLive.PostTest do
   use ExUnit.Case, async: true
   alias MarkdownLive.Post
 
-  @shortcode = "abcd"
-
-  setup do
-    [post: Post.create(@shortcode)]
-  end
-
   test "render/1" do
     input = "# Hello World"
-    assert Post.render(input) == {:ok, "<h1>Hello World</h1>"}
-  end
-
-  test "", %{post: post} do
-    {:ok, rendered} = Post.update(post, "## Hello World")
-    assert rendered == {"<h2>Hello World</h2>"}
+    assert Post.render(input) == {:ok, "<h1>Hello World</h1>\n"}
   end
 end
 
